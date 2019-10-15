caee370e765659fe68cc305a52e529845dd0c54f
Emily Baker-King <ebakerking@gmail.com>
2019-10-15 12:33:29 -0500

    added geocoding function body and added the nav bar to index.html

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,13 @@
 
 (function () {
-    getWeatherInfo();
+    //getWeatherInfo();
+    geocode();
 })();
 
 //Function to connect to the Dark Sky API and get weather data
 function getWeatherInfo() {
 
     //Base-URL/APIKey/Latitude,Longitude
-
     $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/37.8267,-122.4233", { dataType: "jsonp"})
     .done(function(data) {
         console.log(data);
@@ -23,5 +23,17 @@ function getWeatherInfo() {
 
 //Function to connect  tohe MapQuest Geocoding API and get geocoding data
 function geocode() {
+    
+    //Base-URL + APIKeys + &Location= + Address
+    $.ajax("http://www.mapquestapi.com/geocoding/v1/address?key=" + mapQuestKey + "&location=Washington,DC")
+    .done(function(data) {
+        console.log(data);
+    })
+    .fail(function(error) {
+        console.log(error);
+    })
+    .always(function() {
+        console.log("Geocoding call complete!")
+    })
 
 }
